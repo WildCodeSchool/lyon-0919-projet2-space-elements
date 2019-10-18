ec9f8f3f60d0598f02f5d75b7fa67c70090f204b
Farah SELTANY <farahseltany@gmail.com>
2019-10-18 15:16:44 +0200

    Stage code added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OptionsComponent } from './options/options.component';
 import { GameComponent } from './game/game.component';
+import { RouterModule } from '@angular/router';
+import { routes } from './app-routing.module'
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { GameComponent } from './game/game.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
