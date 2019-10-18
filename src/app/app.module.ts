5cbd577bf41b3b2102e31ae0db0aee6fc2a61e4b
Farah SELTANY <farahseltany@gmail.com>
2019-10-18 10:37:17 +0200

    Play button added on option page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OptionsComponent } from './options/options.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OptionsComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
