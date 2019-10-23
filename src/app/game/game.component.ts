414f0eed5682f3a224e3533a783a7a3a5a317d19
Andre DAVIET <andredaviet@hotmail.fr>
2019-10-23 12:44:02 +0200

    ship move

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
+  moveX : number = 250;
+  moveY : number = 250;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  @HostListener('document:keydown', ['$event'])
+  onKeydownHandler(event: KeyboardEvent) {
+    if (event.code === 'ArrowRight') {
+      this.moveX = this.moveX + 12  ;
+    }
+    if (event.code === 'ArrowLeft') {
+      this.moveX = this.moveX - 12  ;
+    }
+    if (event.code === 'ArrowDown') {
+      this.moveY = this.moveY + 12  ;
+    }
+    if (event.code === 'ArrowUp') {
+      this.moveY = this.moveY - 12  ;
+    }
+  }
+  // @HostListener('document:keyup', ['$event'])
+  // onKeyupHandler(event: KeyboardEvent) {
+  //   if (event.code === 'ArrowRight') {
+  //     this.move = false;
+  //     console.log(this.move)
+  //   }
+  // }
 }
