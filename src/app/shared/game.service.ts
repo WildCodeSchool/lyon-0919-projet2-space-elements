0aa0cbc17ccf4400fdf415c9ede2f9cf189891b2
HakimDai <hakim69400@live.com>
2019-11-10 15:52:05 +0100

    import ships pictures

--- src/app/shared/game.service.ts
+++ src/app/shared/game.service.ts
@@ -69,8 +69,8 @@ export class GameService {
     url : '',
     posX: 0,
     posY: 880,
-    height : 60,
-    width : 40,
+    height : 92.232,
+    width : 100,
     size : 0,
     HP: 10,
     type: this.shipTypes[0],
@@ -182,16 +182,16 @@ export class GameService {
     let ammo : Ammo;
     switch(this.ship.type){
       case this.shipTypes[0] :
-        ammo = new Ammo(this.ammoTypes[0], this.ship.posX + 18, this.ship.posY - 10);
+        ammo = new Ammo(this.ammoTypes[0], this.ship.posX + 45.116, this.ship.posY - 10);
         break;
       case this.shipTypes[1] :
-        ammo = new Ammo(this.ammoTypes[1], this.ship.posX + 18, this.ship.posY - 10);
+        ammo = new Ammo(this.ammoTypes[1], this.ship.posX + 45.116, this.ship.posY - 10);
         break;
      case this.shipTypes[2] :
-        ammo = new Ammo(this.ammoTypes[2], this.ship.posX + 18, this.ship.posY - 10);
+        ammo = new Ammo(this.ammoTypes[2], this.ship.posX + 45.116, this.ship.posY - 10);
         break;
      case this.shipTypes[3] :
-        ammo = new Ammo(this.ammoTypes[3], this.ship.posX + 18, this.ship.posY - 10);
+        ammo = new Ammo(this.ammoTypes[3], this.ship.posX + 45.116, this.ship.posY - 10);
         break;
     }
       return this.ammos.add(ammo);
